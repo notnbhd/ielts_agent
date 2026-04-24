ae7e9fb9605297c64d56d5c1f8767214821ca3a3
notnbhd <bachhaidangnguyen@gmail.com>
2026-04-24 16:05:21 +0700

    update : tutor tools

--- test3.py
+++
@@ -1,3 +0,0 @@
-from agent.graph import evaluate_node
-from agent.schemas import IELTSEvaluation
-print("Hello")
--- test_script.py
+++
@@ -1,6 +0,0 @@
-import sys
-import json
-from agent.schemas import IELTSEvaluation, TutorFeedback
-
-# test instantiation
-print("Schemas loaded")
